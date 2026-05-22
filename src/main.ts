04894b3b475e3535b6044939136ccc74420a26dd
Pablo Cocciaglia <cocciaglia@gmail.com>
2026-05-22 05:56:07 +0200

    enable cors for production

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://psicovalgodoy.com/', 'https://www.psicovalgodoy.com/'],
-  });
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
