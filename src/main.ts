5da8e071eb8777d683f5360dff8adb1ed45b7b9f
Pablo Cocciaglia <cocciaglia@gmail.com>
2026-05-22 05:20:38 +0200

    enable oors for production

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://psicovalgodoy.netlify.app/'],
+    origin: [
+      'http://localhost:4200',
+      'https://psicovalgodoy.netlify.app/',
+      'https://psicovalgodoy.com/',
+    ],
   });
 
   app.useGlobalPipes(
